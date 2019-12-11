fcc9e7571888ce74b484b0c724616186630829f0
Michael Kirkpatrick <michael@mekstudios.com>
2019-12-10 21:51:57 -0800

    Update catalog-beer-schema.sql with Billing Table
    Added the billing table to the schema.

--- catalog-beer-schema.sql
+++ catalog-beer-schema.sql
@@ -1,24 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.7.20-0ubuntu0.16.04.1)
-# Database: catalogbeer
-# Generation Time: 2017-11-23 22:45:26 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 # Dump of table api_keys
 # ------------------------------------------------------------
@@ -63,6 +42,18 @@ CREATE TABLE `beer` (
   `cbVerified` tinyint(1) DEFAULT '0',
   `brewerVerified` tinyint(1) DEFAULT '0',
   `lastModified` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# Billing table
+# ------------------------------------------------------------
+
+CREATE TABLE `billing` (
+  `id` varchar(36) NOT NULL,
+  `apiKey` varchar(36) DEFAULT NULL,
+  `freeRemaining` int(4) DEFAULT NULL,
+  `paidCount` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
